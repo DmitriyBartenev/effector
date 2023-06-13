f209753d799309a0b057602a15439956e7a691e0
Dmitriy <bartenevdmitriy@bk.ru>
2023-06-13 18:47:20 +0200

    Added AddEmployee Event

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,12 +1,25 @@
+'use client';
 import React from 'react';
+
+import { useStore } from 'effector-react';
+
+import $store from '../../effector/store';
 
 import styles from './users.module.scss';
 
 const Users: React.FC = () => {
+  const store = useStore($store);
+
   return (
     <div className={styles.usersList}>
       <ul>
-        <li>User Name</li>
+        {store.employees.map((employee) => (
+          <li key={employee.id}>
+            <input type='checkbox' />
+            <span>{employee.fullName}</span>
+            <button>Delete</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
