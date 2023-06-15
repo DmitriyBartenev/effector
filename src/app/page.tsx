efb38a16edcd46e0104059f8e8870a652a6ce322
Dmitriy <bartenevdmitriy@bk.ru>
2023-06-15 16:30:19 +0200

    CreatedThemeContext

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import EmployeeForm from '@components/Forms/EmployeeForm';
 import Users from '@components/Users/Users';
 import Header from '@components/Header/Header';
+import ThemeLayout from '@components/Layouts/ThemeLayout';
 
 export default function Home() {
   return (
     <main>
-      <Header />
-      <EmployeeForm />
-      <Users />
+      <ThemeLayout>
+        <Header />
+        <EmployeeForm />
+        <Users />
+      </ThemeLayout>
     </main>
   );
 }
