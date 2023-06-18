81e632e72b6b640281f357d8e7c4feefb7950dc0
Dmitriy <bartenevdmitriy@bk.ru>
2023-06-18 14:38:59 +0200

    Changed global styles, added bg-color to :root and body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-import EmployeeForm from '@components/Forms/EmployeeForm';
-import Users from '@components/Users/Users';
-import Header from '@components/Header/Header';
 import ThemeProvider from '@components/Layouts/ThemeProvider';
+import Header from '@components/Header/Header';
+import Users from '@components/Users/Users';
 
 export default function Home() {
   return (
-    <main>
-      <ThemeProvider>
-        <Header />
-        <EmployeeForm />
-        <Users />
-      </ThemeProvider>
-    </main>
+    <ThemeProvider>
+      <Header />
+      <Users />
+    </ThemeProvider>
   );
 }
