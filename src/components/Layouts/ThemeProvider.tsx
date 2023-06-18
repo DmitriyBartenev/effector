47a22a703f1fc997ae38e9c962f4dcefab696316
Dmitriy <bartenevdmitriy@bk.ru>
2023-06-18 14:36:00 +0200

    Added ThemeContext, light and dark modes

--- src/components/Layouts/ThemeProvider.tsx
+++ src/components/Layouts/ThemeProvider.tsx
@@ -4,34 +4,57 @@ import React, { createContext } from 'react';
 
 interface IThemeContext {
   activeTheme: string;
-  toggleActiveTheme?: () => void;
+  activeTextTheme: string;
+  toggleActiveTheme: () => void;
 }
 
 const themes = {
-  light: '#f3e8eb',
-  dark: '#433e49',
+  background: {
+    light: '#f3e8eb',
+    dark: '#433e49',
+  },
+  text: {
+    light: '#f3e8eb',
+    dark: '#433e49',
+  },
 };
 
 export const ThemeContext = createContext<IThemeContext>({
-  activeTheme: themes.light,
+  activeTheme: themes.background.light,
+  activeTextTheme: themes.text.light,
+  toggleActiveTheme: () => undefined,
 });
 
 const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [activeTheme, setActiveTheme] = React.useState(themes.light);
+  const [activeTheme, setActiveTheme] = React.useState(themes.background.light);
+  const [activeTextTheme, setActiveTextTheme] = React.useState(
+    themes.text.dark
+  );
 
   const toggleActiveTheme = () => {
-    if (activeTheme === themes.light) {
-      setActiveTheme(themes.dark);
+    if (activeTheme === themes.background.light) {
+      setActiveTheme(themes.background.dark);
+      setActiveTextTheme(themes.text.light);
+      document.body.style.backgroundColor = themes.background.dark;
     } else {
-      setActiveTheme(themes.light);
+      setActiveTheme(themes.background.light);
+      setActiveTextTheme(themes.text.dark);
+      document.body.style.backgroundColor = themes.background.light;
     }
   };
 
+  const mainSectionStyle = {
+    backgroundColor: activeTheme,
+    color: activeTextTheme,
+  };
+
   return (
-    <ThemeContext.Provider value={{ activeTheme, toggleActiveTheme }}>
-      {children}
+    <ThemeContext.Provider
+      value={{ activeTheme, activeTextTheme, toggleActiveTheme }}
+    >
+      <main style={mainSectionStyle}>{children}</main>
     </ThemeContext.Provider>
   );
 };
