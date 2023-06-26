f4371bc4523df70faf11a222c389cf92e0f8602a
Dmitriy <bartenevdmitriy@bk.ru>
2023-06-26 18:43:38 +0200

    Added ui reexport

---
+++ src/components/ui/index.ts
@@ -0,0 +1,19 @@
+import { EmployeeSubmitButton } from './buttons/EmployeeSubmitButton';
+
+import { EmployeeInput } from './inputs/EmployeeInput';
+
+import { DarkThemeIcon } from './icons/DarkThemeIcon';
+import { FavouriteEmployeeIcon } from './icons/FavouriteEmployeeIcon';
+
+export const ui = {
+  buttons: {
+    EmployeeSubmitButton,
+  },
+  inputs: {
+    EmployeeInput,
+  },
+  icons: {
+    DarkThemeIcon,
+    FavouriteEmployeeIcon,
+  },
+};
