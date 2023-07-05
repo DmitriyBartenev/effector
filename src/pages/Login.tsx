928c64a23f5b468393fbece399457406625f0dc2
Dmitriy <bartenevdmitriy@bk.ru>
2023-07-05 15:09:54 +0200

    Added atomic-router and tabler/icons-react

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,28 @@
 import React from 'react';
+import {
+  Anchor,
+  Box,
+  Button,
+  Center,
+  Container,
+  Flex,
+  Group,
+  Modal,
+  Paper,
+  PasswordInput,
+  rem,
+  Space,
+  Text,
+  TextInput,
+  Title,
+} from '@mantine/core';
+import {
+  IconArrowLeft,
+  IconAt,
+  IconFaceId,
+  IconLock,
+} from '@tabler/icons-react';
+import { Link } from 'atomic-router-react';
 
 const Login = () => {
   return <h1>Login</h1>;
