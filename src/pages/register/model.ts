d27ab91a33a7680998da5b1f1ebabdd0b3e4e523
Dmitriy <bartenevdmitriy@bk.ru>
2023-07-11 18:42:18 +0200

    Added route models

---
+++ src/pages/register/model.ts
@@ -0,0 +1,5 @@
+import { routes } from 'shared/routing';
+
+export const currentRoute = routes.auth.register;
+
+currentRoute.open.watch(() => console.log('Register route opened'));
---
+++ src/pages/search/model.ts
@@ -0,0 +1,5 @@
+import { routes } from 'shared/routing';
+
+export const currentRoute = routes.search;
+
+currentRoute.open.watch(() => console.log('Search route opened'));
