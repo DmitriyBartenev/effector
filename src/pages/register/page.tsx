1b503355a608ec6b582227c1c7c14b819f8bb917
Dmitriy <bartenevdmitriy@bk.ru>
2023-07-16 16:32:46 +0200

    Register page layout

--- src/pages/register/page.tsx
+++ src/pages/register/page.tsx
@@ -1,7 +1,51 @@
+import {Anchor, Container, Paper, PasswordInput, Text, TextInput, Title} from '@mantine/core';
+import {IconAt, IconLock, IconPhone, IconUser} from '@tabler/icons-react';
+import {Link} from 'atomic-router-react';
 import React from 'react';
+import {IMaskInput} from 'react-imask';
 
-const RegisterPage = () => {
-  return <h1>Register</h1>;
-};
+import {routes} from '~/shared/routing';
+
+export const RegisterPage = () => (
+  <>
+    <Container size={420} my={40} w="100%" h="100vh">
+      <Title
+        align="center"
+        sx={(theme) => ({fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900})}
+      >
+        Welcome back!
+      </Title>
+      <Text color="dimmed" size="sm" align="center" mt={5}>
+        Already have an account?{' '}
+        <Anchor size="sm" component={Link} to={routes.auth.login}>
+          Log in
+        </Anchor>
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput
+          label="username"
+          placeholder="username"
+          required
+          icon={<IconUser size="0.8rem" />}
+        />
+        <TextInput
+          label="email"
+          placeholder="email"
+          required
+          mt="md"
+          icon={<IconAt size="0.8rem" />}
+        />
+        <PasswordInput
+          label="password"
+          placeholder="your password"
+          required
+          mt="md"
+          icon={<IconLock size="0.8rem" />}
+        />
+      </Paper>
+    </Container>
+  </>
+);
 
 export default RegisterPage;
