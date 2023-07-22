624385c1992e6b8de0606d1a8ff9002690839e9b
Dmitriy <bartenevdmitriy@bk.ru>
2023-07-22 13:54:12 +0200

    Added pageLoader when session is fetching

--- src/pages/search/index.ts
+++ src/pages/search/index.ts
@@ -1,7 +1,17 @@
+import {createRouteView} from 'atomic-router-react';
+
+import {PageLoader} from '~/shared/ui';
+
 import {authorizedRoute, currentRoute} from './model';
 import SearchPage from './page';
 
+const AuthorizedView = createRouteView({
+  route: authorizedRoute,
+  component: SearchPage,
+  otherwise: PageLoader,
+});
+
 export const SearchRoute = {
-  view: SearchPage,
+  view: AuthorizedView,
   route: currentRoute,
 };
