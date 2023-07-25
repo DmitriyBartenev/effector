97c746ca6e27c730f2c3406f0eb28514325fc97a
Dmitriy <bartenevdmitriy@bk.ru>
2023-07-25 11:49:28 +0200

    Added recipe-card

--- src/pages/search/page.tsx
+++ src/pages/search/page.tsx
@@ -1,4 +1,18 @@
-import React from 'react';
+import {
+  Box,
+  Center,
+  Chip,
+  Container,
+  Grid,
+  Group,
+  Loader,
+  NumberInput,
+  Text,
+  TextInput,
+  Title,
+} from '@mantine/core';
+import {useList, useUnit} from 'effector-react';
+import React, {useEffect} from 'react';
 
 export const SearchPage = () => {
   return <h1>Search</h1>;
